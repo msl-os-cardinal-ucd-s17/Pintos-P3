11eb29eab40d20017ee1cde87f2a02bd9d3c325c
brett <brettgedvilas@gmail.com>
2017-03-08 21:13:36 -0700

    wrapped raw int int a fixed_point struct

--- src/threads/fixed-point.h
+++ src/threads/fixed-point.h
@@ -17,76 +17,104 @@ operating system.
 // Define the conversion factor (left shift is equivalent to 2^14)
 #define FACTOR 2<<FRACTIONAL_BITS
 
+/*
+    Wrap the fixed point value into a struct to protect from 
+    conversion/interpretation errors
+ */ 
+struct fixed_point 
+{
+    int value;
+};
+
 
 // Convert integer to fixed point
-inline int int_to_fixed(int integer)
+inline struct fixed_point int_to_fixed(int integer)
 {
-    return integer * FACTOR;
+    struct fixed_point number;
+    number.value = integer * FACTOR;
+    return number;
 }
 
 // Convert fixed point to integer (round toward 0)
-inline int fixed_to_int_round0(int fixed)
+inline int fixed_to_int_round0(struct fixed_point num)
 {
-    return fixed / FACTOR;
+    return num.value/FACTOR;
 } 
 
 // Convert fixed point to integer (round to nearest integer)
-inline int fixed_to_int_roundInt(int fixed)
+inline int fixed_to_int_roundInt(struct fixed_point num)
 {
-    if (fixed >= 0) {
-        return fixed + (FACTOR/2);
+
+    if (num.value >= 0) {
+        return num.value + (FACTOR/2);
     }
     else {
-        return fixed - (FACTOR/2);
+        return num.value - (FACTOR/2);
     }
 }
 
 // Add two fixed point numbers
-inline int add_fixed(int fixed1, int fixed2)
+inline struct fixed_point add_fixed(struct fixed_point fixed1, struct fixed_point fixed2)
 {
-    return fixed1 + fixed2;
+    struct fixed_point number;
+    number.value = fixed1.value + fixed2.value;
+    return number;
 }
 
 // Subtract two fixed point numbers
-inline int sub_fixed(int fixed1, int fixed2)
+inline struct fixed_point sub_fixed(struct fixed_point fixed1, struct fixed_point fixed2)
 {
-    return fixed1 - fixed2;
+    struct fixed_point number;
+    number.value = fixed1.value - fixed2.value;
+    return number;
 }
 
 // Add an integer to a fixed point number
-inline int fixed_plus_int(int fixed, int integer)
+inline struct fixed_point fixed_plus_int(struct fixed_point fixed, int integer)
 {
-    return fixed + integer * FACTOR;
+    struct fixed_point number;
+    number.value = fixed.value + (integer * FACTOR);
+    return number;
 }
 
 // Subtract integer from fixed point number
-inline int fixed_minus_int(int fixed, int integer)
+inline struct fixed_point fixed_minus_int(struct fixed_point fixed, int integer)
 {
-    return fixed - (integer * FACTOR);
+    struct fixed_point number;
+    number.value = fixed.value - (integer * FACTOR);
+    return number;
 }
 
 // Multiply two fixed integers
-inline int mult_fixed(int fixed1, int fixed2)
+inline struct fixed_point mult_fixed(struct fixed_point fixed1, struct fixed_point fixed2)
 {
-    return ((int64_t)fixed1) * fixed2/FACTOR;
+    struct fixed_point number;
+    number.value = ((int64_t)fixed1.value) * fixed2.value/FACTOR;
+    return number;
 }
 
 // multiply fixed and int
-inline int fixed_mult_int(int fixed, int integer)
+inline struct fixed_point fixed_mult_int(struct fixed_point fixed, int integer)
 {
-    return fixed * integer;
+    struct fixed_point number;
+    number.value = fixed.value * integer;
+    return number;
 }
 
 // divide two fixed point numbers
-inline int div_fixed(int fixed1, int fixed2)
+inline struct fixed_point div_fixed(struct fixed_point fixed1, struct fixed_point fixed2)
 {
-    return ((int64_t)fixed1) * FACTOR/fixed2;
+    struct fixed_point number;
+    number.value = ((int64_t)fixed1.value) * FACTOR/fixed2.value;
+    return number;
 }
 
 // divide fixed by int
-inline int fixed_div_int(int fixed, int integer)
+inline struct fixed_point fixed_div_int(struct fixed_point fixed, int integer)
 {
-    return fixed/integer;
+    struct fixed_point number;
+    number.value = fixed.value/integer;
+    return number;
 }
 
 
