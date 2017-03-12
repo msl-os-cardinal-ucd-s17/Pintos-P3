ffe7fef29ca227191e247246ce13cf1b5a5cb0d9
Rachel Popo <rach.popo@gmail.com>
2017-03-11 22:51:15 -0700

    Update thread.c

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -612,6 +612,10 @@ init_thread (struct thread *t, const char *name, int priority)
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
+  
+  t->nice = 0;
+  t->recent_cpu = 0;
+  
 }
 
 /* Allocates a SIZE-byte frame at the top of thread T's stack and
