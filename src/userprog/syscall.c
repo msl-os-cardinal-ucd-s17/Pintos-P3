825339c587264e7f892f4de0a6dfe589c6740592
brett <brettgedvilas@gmail.com>
2017-04-05 19:22:29 -0600

    Adding syscall handler infrastructure

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,7 +8,7 @@
 #include "devices/shutdown.h"
 #include "lib/user/syscall.h"
 
-#define arg0    ((f->esp)+4)
+#define arg0  ((f->esp)+4)
 #define arg1	((f->esp)+8)
 #define arg2	((f->esp)+12)
 #include "threads/malloc.h"
@@ -17,13 +17,21 @@
 static void syscall_handler (struct intr_frame *);
 // static int get_user (const uint8_t *uaddr);
 bool verify_user_ptr(void*vaddr);
+
+/* System call function prototypes */
 void system_halt(void);
 void system_exit(int status);
 pid_t system_exec(const char*cmd_line);
+int system_wait(pid_t pid);
+bool system_create(const char *file, unsigned initial_size);
+bool system_remove(const char *file);
 int system_open(const char *file);
-void system_close(int fd);
+int system_filesize(int fd);
+int system_read(int fd, void *buffer, unsigned size);
 int system_write(int fd, const void *buffer, unsigned size);
-pid_t system_exec(const char*cmd_line);
+void system_seek(int fd, unsigned position);
+unsigned system_tell(int fd);
+void system_close(int fd);
 
 static struct fd_elem* find_fd(int fd);
 static int next_fd(void);
@@ -95,30 +103,40 @@ syscall_handler (struct intr_frame *f UNUSED)
   			system_halt();
   			break;
   		case SYS_EXIT:
-  			system_exit(*((int*)arg0));
+  			system_exit(*(int*)arg0);
   			break;
   		case SYS_EXEC:
+        system_exec((char*)arg0);
   			break;
   		case SYS_WAIT:
+        system_wait(*(pid_t*)arg0);
   			break;
   		case SYS_CREATE:
+        system_create((char*)arg0, *(unsigned*)arg1);
   			break;
   		case SYS_REMOVE:
+        system_remove((char*)arg0);
   			break;
   		case SYS_OPEN:
+        system_open((char*)arg0);//(const char *file);
   			break;
   		case SYS_FILESIZE:
-  			break;
-  		case SYS_READ:
+        system_filesize(*(int*)arg0);
+        break;
+      case SYS_READ:
+        system_read(*(int*)arg0, arg1, *(unsigned*)arg2);
   			break;
   		case SYS_WRITE:
         system_write(*((int*)arg0), arg1, *((unsigned*)arg2));
   			break;
   		case SYS_SEEK:
+        system_seek(*(int*)arg0, *(unsigned*)arg1);    
   			break;
   		case SYS_TELL:
+        system_tell(*(int*)arg0);
   			break;
   		case SYS_CLOSE:
+        system_close(*(int*)arg0); //(int fd);
   			break;
 		default:
 			break;
@@ -128,12 +146,33 @@ syscall_handler (struct intr_frame *f UNUSED)
 }
 
 void system_halt(void) {
+  /* Terminates pintos */
 	shutdown_power_off();
 }
 
 void system_exit(int status) {
     // printf("Status Number: %d\n", status);
+    /* Terminates current user program and returns status to the kernel */
     thread_exit();
+}
+
+pid_t system_exec(const char*cmd_line){
+  /* runs executable given by cmd_line */
+  pid_t id;
+  return(id);
+}
+
+int system_wait(pid_t pid) {
+  printf("sys_wait not implemented");
+}
+
+bool system_create(const char *file, unsigned initial_size) {
+  printf("sys_create not implemented");
+
+}
+
+bool system_remove(const char *file) {
+  printf("sys_remove not implemented");
 }
 
 int system_open(const char *file){
@@ -160,16 +199,12 @@ int system_open(const char *file){
 
 }
 
-void system_close(int fd){
-   if (find_fd(fd) != NULL){
-      struct fd_elem *fde = find_fd(fd);
-      // Close file
-      file_close(fde->file);
-      // Remove from open file list
-      list_remove(&fde->elem);
-      // Free memory
-      free(fde);
-   }
+int system_filesize(int fd) {
+  printf("sys_filesize not implemented");
+}
+
+int system_read(int fd, void *buffer, unsigned size) {
+  printf("sys_read not implemented");
 }
 
 int system_write(int fd, const void *buffer, unsigned size){
@@ -199,9 +234,24 @@ int system_write(int fd, const void *buffer, unsigned size){
    }
 }
 
-pid_t system_exec(const char*cmd_line){
-	pid_t id;
-	return(id);
+void system_seek(int fd, unsigned position) {
+  printf("sys_seek not implemented");
+}
+
+unsigned system_tell(int fd) {
+  printf("sys_tell not implemented");
+}
+
+void system_close(int fd){
+   if (find_fd(fd) != NULL){
+      struct fd_elem *fde = find_fd(fd);
+      // Close file
+      file_close(fde->file);
+      // Remove from open file list
+      list_remove(&fde->elem);
+      // Free memory
+      free(fde);
+   }
 }
 
 bool verify_user_ptr(void *vaddr) {
