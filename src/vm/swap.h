3bb4a6fc941ad4e0274bb751e95ccd2e2418ea3c
rachelpopo <rach.popo@gmail.com>
2017-04-29 12:42:07 -0600

    updated swap

--- src/vm/swap.h
+++ src/vm/swap.h
@@ -1,6 +1,14 @@
 #ifndef SWAP_H
 #define SWAP_H
 
+#include <bitmap.h>
+#include "devices/block.h"
 
+struct page;
+
+
+void init_swap(void);
+void swap_out(void *frame);
+void swap_in(void *frame);
 
 #endif // SWAP_H
